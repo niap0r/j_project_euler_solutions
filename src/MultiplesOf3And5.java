8de952821378bd5fd093d5844f26ed4d6c1b779a
niap0r <zapf1@protonmail.com>
2020-07-02 16:19:06 +0200

    Initial commit

---
+++ src/MultiplesOf3And5.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultiplesOf3And5 {
+    // first problem of project euler
+
+    byte multi1 = 3;
+    byte multi2 = 5;
+    int limit = 1000;
+
+    public int sumOfMultiples() {
+        ArrayList<Integer> multiples = new ArrayList<>();
+        for (int i = 1; i < limit; i++) {
+            if (i % multi1 == 0 || i % multi2 == 0) {
+                multiples.add(i);
+            }
+        }
+        return sumArrayList(multiples);
+    }
+
+    private int sumArrayList(ArrayList<Integer> multiples) {
+        int sum = 0;
+        for (int n : multiples) {
+            sum += n;
+        }
+        return sum;
+    }
+
+
+}
---
+++ src/Program.java
@@ -0,0 +1,6 @@
+public class Program {
+    public static void main(String[] args) {
+        MultiplesOf3And5 multiplier = new MultiplesOf3And5();
+        System.out.println(multiplier.sumOfMultiples());
+    }
+}
